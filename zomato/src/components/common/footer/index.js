241f61503d08f5f2bbfca79c85e75f0468aca606
manjunathkulkarni <manjunathkulkarni02@gmail.com>
2024-02-01 21:45:22 +0530

    removed explore

--- zomato/src/components/common/footer/index.js
+++ zomato/src/components/common/footer/index.js
@@ -1,10 +1,9 @@
-import ExploreOptions from "./Exploreoption";
 import PopularLocalities from "./popularLocalities";
+
 const Footer = () => {
   return (
     <>
       <PopularLocalities />
-      <ExploreOptions />
     </>
   );
 };
